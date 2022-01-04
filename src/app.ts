34848256d899f55dc691c21cac1bbcab87ea1208
Leonardo Freedy <leonardo.freedy.webart@gmail.com>
2022-01-04 17:25:09 -0300

    Adiciona scroll auto nas options do select e estilos personalizado

--- src/app.ts
+++ src/app.ts
@@ -17,58 +17,60 @@ class App {
     this.template = `
       <link rel="stylesheet" href="https://leofredy.github.io/bot-umentor/src/assets/styles/style.css">
       <link rel="stylesheet" href="https://leofredy.github.io/bot-umentor/src/assets/styles/animations.css">
-      <div class="tonDroid">
-        <div class="tonDroid-controls">
-          <button class="controls-minimize">
-            -
-          </button>
-          <button class="controls-close">
-            <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" fill="#89898B"/>
-            </svg>
-          </button>
+      <body>
+        <div class="tonDroid">
+          <div class="tonDroid-controls">
+            <button class="controls-minimize">
+              -
+            </button>
+            <button class="controls-close">
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <path d="M19 6.41L17.59 5L12 10.59L6.41 5L5 6.41L10.59 12L5 17.59L6.41 19L12 13.41L17.59 19L19 17.59L13.41 12L19 6.41Z" fill="#89898B"/>
+              </svg>
+            </button>
+          </div>
+          <header>
+            <img 
+              src="https://leofredy.github.io/bot-umentor/src/assets/icon.png" 
+              alt="Ton Droid, bot para realizar curso umentor"
+            >
+          </header>
+          <div class="tonDroid-contents">
+            <div class="tonDroid-content">
+              <h2 class="content-title">
+                Fazer prova
+              </h2>
+              <div class="content-toggle">
+                <input id="toggleFazerProva" type="checkbox">
+                <label for="toggleFazerProva"></label>
+              </div>
+            </div>
+
+            <div class="tonDroid-content">
+              <h2 class="content-title">
+                Finalizar módulos
+              </h2>
+
+              ${this.selectTemplate}
+
+              <div class="content-toggle">
+                <input id="toggleModulos" type="checkbox">
+                <label for="toggleModulos"></label>
+              </div>
+            </div>
+
+            <div class="tonDroid-content">
+              <h2 class="content-title">
+                Finalizar curso
+              </h2>
+              <div class="content-toggle">
+                <input id="toggleCurso" type="checkbox">
+                <label for="toggleCurso"></label>
+              </div>
+            </div>
+          </div>
         </div>
-        <header>
-          <img 
-            src="https://leofredy.github.io/bot-umentor/src/assets/icon.png" 
-            alt="Ton Droid, bot para realizar curso umentor"
-          >
-        </header>
-        <div class="tonDroid-contents">
-          <div class="tonDroid-content">
-            <h2 class="content-title">
-              Fazer prova
-            </h2>
-            <div class="content-toggle">
-              <input id="toggleFazerProva" type="checkbox">
-              <label for="toggleFazerProva"></label>
-            </div>
-          </div>
-
-          <div class="tonDroid-content">
-            <h2 class="content-title">
-              Finalizar módulos
-            </h2>
-
-            ${this.selectTemplate}
-
-            <div class="content-toggle">
-              <input id="toggleModulos" type="checkbox">
-              <label for="toggleModulos"></label>
-            </div>
-          </div>
-
-          <div class="tonDroid-content">
-            <h2 class="content-title">
-              Finalizar curso
-            </h2>
-            <div class="content-toggle">
-              <input id="toggleCurso" type="checkbox">
-              <label for="toggleCurso"></label>
-            </div>
-          </div>
-        </div>
-      </div>
+      </body>
     `;
     
     this.containerApp.innerHTML = this.template;
