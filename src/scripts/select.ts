bd2cd780a1af525262b94063b84580aaed4ff09f
Leonardo Freedy <leonardo.freedy.webart@gmail.com>
2022-01-07 12:33:12 -0300

    Corrige changeEvent para quando não houver modulos a serem concluido

--- src/scripts/select.ts
+++ src/scripts/select.ts
@@ -94,10 +94,12 @@ class Select {
 
   private changeEvent(event: Event): void {
     const valueTarget = (event.currentTarget as HTMLElement).children[1].getAttribute("value")!;
-    if (this.listModulosConcluidos.indexOf(valueTarget) === -1) {
-      this.value = valueTarget;
-      this.selectDOM.children[0].children[0].innerHTML = this.value; 
-      this.updateListOptions();
+    if (valueTarget) {
+      if (this.listModulosConcluidos.indexOf(valueTarget) === -1) {
+        this.value = valueTarget;
+        this.selectDOM.children[0].children[0].innerHTML = this.value; 
+        this.updateListOptions();
+      }
     }
     this.toggleEvent();
   }
