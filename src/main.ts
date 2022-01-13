9ddf2aa8e443e6d2a60e7ed5091ae7a223f3054c
Leonardo Freedy <leonardo.freedy.webart@gmail.com>
2022-01-12 23:21:20 -0300

    Remove debugg de request

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,3 @@ import App from "./app.js";
 
 const app = new App(curso);
 app.init();
-
-for (let i = 0; i < 2000; i++) {
-  $.post(
-    `https://painel.umentor.com.br/painel_candidato/videos_aulas/controle_cursos`, {
-    indice: 225,
-    curso: 45,
-    operacao: 2,
-    codigo_trilha: codigo_trilha
-  }, function () {
-  })
-}
